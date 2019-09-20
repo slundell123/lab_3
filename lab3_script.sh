c7e4f8189bdbea73067f6de82683e86d1366f15c
slundell123 <salu3240@colorado.edu>
2019-09-20 15:35:41 -0600

    rewrite email file, not append

--- lab3_script.sh
+++ lab3_script.sh
@@ -13,7 +13,7 @@ echo "Number of phone numbers in file: "
 egrep "[0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]$" $filename | wc -l
 echo "Phone numbers with 303 area code:"
 egrep "303-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]$" $filename
-egrep $search $filename >> email_results.txt
+egrep $search $filename > email_results.txt
 echo "Number of emails: "
 egrep $search $filename | wc -l
 
