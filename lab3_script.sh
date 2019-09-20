1560d9a02a37f99b72337ff14c45e8e24f81fe93
slundell123 <salu3240@colorado.edu>
2019-09-20 15:27:52 -0600

    Adding phone numbers to the command

--- lab3_script.sh
+++ lab3_script.sh
@@ -8,6 +8,12 @@
 echo "Enter a file name: "
 read filename
 echo "Enter a regular expression to search file with: "
-read search
+read search #pattern: [A-Z0-9._%+-]+@geocities.com
+echo "Number of phone numbers in file: "
+egrep "[0-9][0-9][0-9].[0-9][0-9][0-9].[0-9][0-9][0-9][0-9]$" $filename | wc -l
+echo "Phone numbers with 303 area code:"
+egrep "303.[0-9][0-9][0-9].[0-9][0-9][0-9][0-9]$" $filename
 egrep $search $filename >> email_results.txt
+echo "Number of emails: "
+egrep $search $filename | wc -l
 
